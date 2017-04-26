31034bfc0c4d30cadd686c021d77fb1998a1ac79
dabeli <jumbodabeli@gmail.com>
2017-04-26 14:18:52 +0530

    Last minute udp changes

--- Udp analysis /MyUDP.tcl
+++ Udp analysis /MyUDP.tcl
@@ -1,16 +1,16 @@
-set val(chan)		Channel/WirelessChannel;
+set val(chan)		Channel/Wirelesschannel;
 set val(prop)		Propagation/TwoRayGround;
 set val(netif)		Phy/WirelessPhy;
 set val(ant)		Antenna/OmniAntenna;
-set val(mac)		Mac/802_11;
+set val(mac)		MAC/802_11;
 set val(ll)		LL;
 set val(rp)		AODV;
-set val(ifq)		Queue/DropTail/PriQueue;
+set val(ifq)		Queue/Droptail/PriQueue;
 set val(ifqlen)		50;
 set val(nn)		10;
 set val(x)		500;
 set val(y)		400;
-set val(start)		5;
+set val(start)		35;
 set val(stop)		25;
 
 set ns [new Simulator]
@@ -35,29 +35,29 @@ $ns node-config	-adhocRouting $val(rp)	\
 		-llType $val(ll)	\
 		-antType $val(ant)	\
 		-topoInstance $topo 	\
-		-agentTrace ON		\
-		-routerTrace ON		\
-		-macTrace OFF		\
+		-agentTrace OFF		\
+		-routerTrace OFF		\
+		-macTrace ON		
 		-movementTrace ON
 
 for {set i 0} {$i<$val(nn)} {incr i} {
 	set node($i) [$ns node]
-	$node($i) set X_ [expr 10+round(rand()*480)]
-	$node($i) set Y_ [expr 10+round(rand()*380)]
+	$node($i) set X_ (expr 10+round(rand()*480))
+	$node($i) set Y_ (expr 10+round(rand()*380))
 	$node($i) set Z_ 0.0
 	$ns initial_node_pos $node($i) 30
 	$ns at val(stop) "$node($i) reset"
 }
 
-for {set i 0} {$i<[expr $val(nn)/2]} {incr i} {
+for {set i 0} {$i<(expr $val(nn)/2)} {incr i} {
 	$ns color 1 red
 	set udp($i) [new Agent/UDP]
 	$udp($i) set class_ 2
 	$udp($i) set fid_ 1
 	$ns attach-agent $node($i) $udp($i)
-	set null([expr $val(nn)-$i-1]) [new Agent/Null]
-	$ns attach-agent $node([expr $val(nn)-$i-1]) $null([expr $val(nn)-$i-1])
-	$ns connect $udp($i) $null([expr $val(nn)-$i-1])
+	set null(expr $val(nn)-$i-1) [new Agent/Null]
+	$ns attach-agent $node(expr $val(nn)-$i-1) $null(expr $val(nn)-$i-1)
+	$ns connect $udp($i) $null(expr $val(nn)-$i-1)
 
 	set cbr($i) [new Application/Traffic/CBR]
 	$cbr($i) attach-agent $udp($i)
@@ -71,14 +71,9 @@ $ns at $val(stop) "finish"
 
 proc finish {} {
 	global ns tracefd namtrace
-	$ns flush-trace
 	close $tracefd
 	close $namtrace
 	puts "Simulation end"
-	$ns halt
-	puts "awk -f MyUDPana.awk MyUDP.tr"
-	exec nam MyUDP.nam &
-	exit 0
 }
 
 $ns run
